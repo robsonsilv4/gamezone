8fcf176f946ba602ef5207a124edcba30160d7b6
robsonsilv4 <robsonsilv410@gmail.com>
2020-01-16 19:03:34 -0300

    Adicionado navegação

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
 import React from 'react';
 import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({ navigation }) {
+  const pressHandler = () => {
+    navigation.navigate('ReviewDetails');
+    // navigation.push('ReviewDetails'); // Adiciona novamente?
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Página inicial</Text>
+      <Button title="Ir para avaliações" onPress={pressHandler} />
     </View>
   );
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
 import React from 'react';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetail() {
+export default function ReviewDetail({ navigation }) {
+  const pressHandler = () => {
+    navigation.goBack();
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text>Avaliações</Text>
+      <Button title="Voltar para o inicio" onPress={pressHandler} />
     </View>
   );
 }
