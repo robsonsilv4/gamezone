7490b4bc763d5b5c89c0024bd0048e356ae0ea51
robsonsilv4 <robsonsilv410@gmail.com>
2020-01-18 15:36:58 -0300

    Adicionado imagens dinâmicas

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,32 @@
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 
 import Card from '../shared/card';
 import React from 'react';
-import { globalStyles } from '../styles/global';
 
 export default function ReviewDetail({ navigation }) {
+  const rating = navigation.getParam('rating');
+
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>{navigation.getParam('title')}</Text>
         <Text>{navigation.getParam('body')}</Text>
-        <Text>{navigation.getParam('rating')}</Text>
+        <View style={styles.rating}>
+          <Text>Avaliação GameZone: </Text>
+          <Image source={images.rating[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  },
+});
